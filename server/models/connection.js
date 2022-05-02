707d9de5c5a1853ed6883de34dd017206f19b65b
hugomeduarte <73663723+hugomeduarte@users.noreply.github.com>
2022-05-02 02:57:12 +0100

    Update connection.js

--- server/models/connection.js
+++ server/models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-const connectionString = "postgres://postgres:password@localhost:5432/MyCardio+HSM"
+const connectionString = "postgres://qxykafyzfkrnin:6520a72902ea212e84fc241cfcbdfea8f16b20038640c6cb4b5cd0b6385f7b7f@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d8lp3ms21a309k"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
